bdc87180bda9f9193b4ad0e2cb48db3dca3b0367
Aravind170425 <aravindbabaateam@gmail.com>
2026-01-22 15:51:52 +0530

    fin

--- chola/src/pages/DashboardLayout.jsx
+++ chola/src/pages/DashboardLayout.jsx
@@ -2,13 +2,11 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { DashboardSidebar } from "../componet/Dashboard/DashboardSidebar";
-import DummyNav from "./DummyNav";
 
 export function DashboardLayout() {
   return (
-  <Box sx={{   }}>
-  {/* <DashboardSidebar />
-<DummyNav/> */}
+  <Box sx={{ display: "flex", minHeight: "100vh", background: "#f5f6fa" }}>
+  {/* <DashboardSidebar /> */}
   <Box sx={{ flex: 1, background: "#f5f6fa" }}>
     <Outlet />
   </Box>
